fedbb906815955fd78da6f4d526911650f9595d5
AnaAssuncao <apoassuncao@gmail.com>
2020-12-27 12:40:50 -0300

    Implementação da lista de modalidades

--- JS/Ciranda.js
+++ JS/Ciranda.js
@@ -8,16 +8,16 @@ const menu ={
 }
 menu.listItems.forEach((item)=>
 item.addEventListener("click", () =>{
-    selectItemList(item);
+    selectItemListMenu(item);
 }));
 
 // ITEM MENU SELECT 
-function selectItemList(item){
+function selectItemListMenu(item){
     if(menu.itemSelect!==null){
-        menu.itemSelect.classList.remove("select");
+        menu.itemSelect.classList.remove("menu__select");
     }
         menu.itemSelect = item;
-        item.classList.add("select");
+        item.classList.add("menu__select");
 }
 
 // MENU MAX WIDTH 1000PX
@@ -39,7 +39,7 @@ function openMenu(menu){
         itemList.classList.add("menu__items--width");
         itemList.classList.add("flex__spacearound");
         itemList.addEventListener("click", () =>{
-            selectItemList(itemList);
+            selectItemListMenu(itemList);
         });
         const linkList = document.createElement('a');
         linkList.classList.add("menu__link--width");
@@ -60,37 +60,76 @@ function openMenu(menu){
     }
 }
 
-// dance
+// DANCER
 const balletDance = {
     section: document.querySelector("#dance"),
     img:["BalletDance1","BalletDance2","BalletDance3","BalletDance4","BalletDance5","BalletDance6","BalletDance7"],
     elementImg: []
 }
-teste(0);
-function teste (index){
+
+controlImage(0);
+function controlImage (index){
     renderDanceBallet(balletDance.img[index]);
     renderDanceBallet(balletDance.img[index+1]);
     renderDanceBallet(balletDance.img[index+2]);
      setTimeout((()=>{
         balletDance.elementImg=derenderDanceBallet(balletDance);
-            teste(index)
+        controlImage(index);
         }),5000);
 }
 
 function renderDanceBallet(imgDancer){
-    const a = document.createElement('img');
-    a.classList.add("dance__balletImg");
-    a.src=`Imagens/${imgDancer}.png`;
-    balletDance.elementImg.push(a);
-    balletDance.section.appendChild(a);
+    const elementImgDancer = document.createElement('img');
+    elementImgDancer.classList.add("dance__balletImg");
+    elementImgDancer.src=`Imagens/${imgDancer}.png`;
+    balletDance.elementImg.push(elementImgDancer);
+    balletDance.section.appendChild(elementImgDancer);
 }
 
 function derenderDanceBallet(balletDance){
-    debugger;
     balletDance.elementImg.forEach((imgDancer)=>{
         balletDance.section.removeChild(imgDancer);
     });
-    balletDance.img.push(balletDance.img[0])
-    balletDance.img.shift() 
+    balletDance.img.push(balletDance.img[0]);
+    balletDance.img.shift() ;
     return elementsImg = [];
+}
+
+// MODALITIES
+const modalities = {
+    img:["ModBaby.png","ModBalletInf.jpg",],
+    id: ["modBaby", "modBalletInf","modBalletJuv","modBalletAdul","modJazzInf","modJazzJuv","modJazzAdul",
+    "modStreetDance", "modContemporaryDance"],
+    title:["Baby Ballet","Ballet Infantil","Ballet Juvenil","Ballet Adulto","Jazz Infantil","Jazz Juvenil",
+    "Jazz Adulto","Dança Urbana","Dança Contemporânea"],
+    age:["Entre 3 e 5 anos","Entre 6 e 10 anos","Entre 11 e 14 anos","A partir de 14 anos",
+    "Entre 6 e 10 anos","Entre 11 e 14 anos","A partir de 14 anos",
+    "A partir de 14 anos","A partir de 14 anos"],
+    explanation:["Para as crianças que sonham em ser balarinas desde novas, o Baby Ballet é o primeiro passo para a contato com o Ballet.<br><br>Nesta modalidade elas descobrem a dança de uma maneira lúdica e afetiva. As aulas são divertidas e encantadoras tornando o aprendizado leve e eficaz.",
+"..."],
+    elementSelect:null
+}
+
+creatListModalities(modalities);
+function creatListModalities(modalities){
+    const modalitiesList = document.querySelector("#modalities__list");
+   modalities.id.forEach((id, index)=>{
+    const elementItemList = document.createElement('div');
+    elementItemList.classList.add("modalities__item");
+    elementItemList.innerHTML = modalities.title[index];
+    elementItemList.id = modalities.id[index];
+    elementItemList.onclick= () => selectItemListModalities(elementItemList);;
+    modalitiesList.appendChild(elementItemList);
+    if(!index){
+        elementItemList.classList.add("modalities__select");
+        modalities.elementSelect = elementItemList;
+    }
+   })
+}
+
+// ITEM MENU SELECT 
+function selectItemListModalities(element){
+        modalities.elementSelect.classList.remove("modalities__select");
+        modalities.elementSelect = element;
+        element.classList.add("modalities__select");
 }
