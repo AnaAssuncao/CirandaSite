9b4ebb951f0ff3151f726e4aca534f33fb613ea8
AnaAssuncao <apoassuncao@gmail.com>
2020-12-26 21:08:58 -0300

    Implemantação da seleção dos itens da lista

--- JS/Ciranda.js
+++ JS/Ciranda.js
@@ -1,43 +1,61 @@
-// MENU CONTEXTO
-const clicked ={
-    isClicked:false,
-    items:["SOBRE", "ENSINO", "PROFESSORES","MODALIDADES", "VOZ DOS ALUNOS"]
+const menu ={
+    items:["SOBRE", "ENSINO", "PROFESSORES","MODALIDADES", "VOZ DOS ALUNOS"],
+    itemsID:["SOBRE", "ENSINO", "PROFESSORES","MODALIDADES", "VOZDOSALUNOS"],
+    itemSelect: null,
+    isClickedButtom:false,
+    screenSize:1000,
+    listItems: Array.from(document.querySelectorAll(".menu__items"))
 }
+menu.listItems.forEach((item)=>
+item.addEventListener("click", () =>{
+    selectItemList(item);
+}));
+
+// ITEM MENU SELECT 
+function selectItemList(item){
+    if(menu.itemSelect!==null){
+        menu.itemSelect.classList.remove("select");
+    }
+        menu.itemSelect = item;
+        item.classList.add("select");
+}
+
+// MENU MAX WIDTH 1000PX
 document.querySelector("#header__list").addEventListener("click", () =>{
-    openMenu(clicked);
+    openMenu(menu);
 }); 
 
-function openMenu(clicked){
+function openMenu(menu){
     const header = document.querySelector("#header");
-    if(clicked.isClicked==false){
-    const div = document.createElement('div');
-    div.classList.add("header__menu--small")
-    const ul = document.createElement('ul');
-    ul.classList.add("menu");
-    ul.classList.add("flex__spacearound");
+    if(menu.isClickedButtom===false){
+    const menuHeader = document.createElement('div');
+    menuHeader.classList.add("header__menu--width")
+    const list = document.createElement('ul');
+    list.classList.add("menu");
+    list.classList.add("flex__spacearound");
 
-    for(let i in clicked.items){
-        const li = document.createElement('li');
-        li.classList.add("menu__items--small");
-        li.classList.add("flex__spacearound");
-        
-        const a = document.createElement('a');
-        a.classList.add("menu__link--small");
-        a.innerHTML=clicked.items[i];
-        a.href=`#${clicked.items[i]}`;
+    for(let item in menu.items){
+        const itemList = document.createElement('li');
+        itemList.classList.add("menu__items--width");
+        itemList.classList.add("flex__spacearound");
+        itemList.addEventListener("click", () =>{
+            selectItemList(itemList);
+        });
+        const linkList = document.createElement('a');
+        linkList.classList.add("menu__link--width");
+        linkList.innerHTML=menu.items[item];
+        linkList.href=`#${menu.itemsID[item]}`;
 
-        li.appendChild(a);
-        ul.appendChild(li);
+        itemList.appendChild(linkList);
+        list.appendChild(itemList);
     }
-    div.appendChild(ul);
-    header.appendChild(div)
-        clicked.isClicked=true;
+    menuHeader.appendChild(list);
+    header.appendChild(menuHeader)
+        menu.isClickedButtom=true;
     }
-    else{
-        
-        const divEssa = document.querySelector(".header__menu--small");
-        header.removeChild(divEssa);
-        clicked.isClicked=false;
-
+    else{    
+        const menuDisplayed = document.querySelector(".header__menu--width");
+        header.removeChild(menuDisplayed);
+        menu.isClickedButtom=false;
     }
 }
