959b09ea9ecc09ec89946a8875a1eae4069a9298
AnaAssuncao <apoassuncao@gmail.com>
2020-12-28 00:35:17 -0300

    Alterações para implementação dos dados das modalidades, com adição de imagens

--- JS/Ciranda.js
+++ JS/Ciranda.js
@@ -97,20 +97,31 @@ function derenderDanceBallet(balletDance){
 
 // MODALITIES
 const modalities = {
-    img:["ModBaby.png","ModBalletInf.jpg",],
+    img:["ModBaby.png","ModBalletInf.jpg","ModBalletJuv.jpg","ModBalletAdult.jpg","ModJazzInf.jpg","ModJazzJuv.jpg",
+    "ModJazzAdult.jpg","ModStreetDance.jpeg"],
     id: ["modBaby", "modBalletInf","modBalletJuv","modBalletAdul","modJazzInf","modJazzJuv","modJazzAdul",
-    "modStreetDance", "modContemporaryDance"],
-    title:["Baby Ballet","Ballet Infantil","Ballet Juvenil","Ballet Adulto","Jazz Infantil","Jazz Juvenil",
-    "Jazz Adulto","Dança Urbana","Dança Contemporânea"],
+    "modStreetDance"],
+    title:["Ballet Baby","Ballet Infantil","Ballet Juvenil","Ballet Adulto","Jazz Infantil","Jazz Juvenil",
+    "Jazz Adulto","Dança Urbana"],
     age:["Entre 3 e 5 anos","Entre 6 e 10 anos","Entre 11 e 14 anos","A partir de 14 anos",
     "Entre 6 e 10 anos","Entre 11 e 14 anos","A partir de 14 anos",
-    "A partir de 14 anos","A partir de 14 anos"],
+    "A partir de 14 anos"],
     explanation:["Para as crianças que sonham em ser balarinas desde novas, o Baby Ballet é o primeiro passo para a contato com o Ballet.<br><br>Nesta modalidade elas descobrem a dança de uma maneira lúdica e afetiva. As aulas são divertidas e encantadoras tornando o aprendizado leve e eficaz.",
+"...",
+"...",
+"...",
+"...",
+"...",
+"...",
 "..."],
-    elementSelect:null
+    elementSelect:null,
+    structureDescription: ["title","age","explanation"],
+    elementCreateDescription: ["h1", "p","p"],
+    addClass:["modalities__title","modalities__age","modalities__explanation"]
 }
 
 creatListModalities(modalities);
+illustrateModalityInformation(document.querySelector("#modalities__group"),0,modalities);
 function creatListModalities(modalities){
     const modalitiesList = document.querySelector("#modalities__list");
    modalities.id.forEach((id, index)=>{
@@ -118,7 +129,10 @@ function creatListModalities(modalities){
     elementItemList.classList.add("modalities__item");
     elementItemList.innerHTML = modalities.title[index];
     elementItemList.id = modalities.id[index];
-    elementItemList.onclick= () => selectItemListModalities(elementItemList);;
+    elementItemList.onclick= (e) => {
+        selectItemListModalities(elementItemList);
+        updateModalityGroup(modalities,e.target)
+    };
     modalitiesList.appendChild(elementItemList);
     if(!index){
         elementItemList.classList.add("modalities__select");
@@ -132,4 +146,46 @@ function selectItemListModalities(element){
         modalities.elementSelect.classList.remove("modalities__select");
         modalities.elementSelect = element;
         element.classList.add("modalities__select");
+}
+
+function updateModalityGroup(modalities,objEvent){
+    const modalitiesGroup = document.querySelector("#modalities__group");
+    removeModalityInformation(modalitiesGroup);
+    const numberIndex= modalities.id.indexOf(objEvent.id);
+    illustrateModalityInformation(modalitiesGroup,numberIndex,modalities);
+}
+
+function  removeModalityInformation(modalitiesGroup){
+
+    modalitiesGroup.removeChild(document.querySelector(".modalities__information"));
+}
+
+function illustrateModalityInformation(modalitiesGroup,numberIndex,modalities){
+        
+    const modGroupInformation = document.createElement('div');
+    modGroupInformation.classList.add("modalities__information");
+    modalitiesGroup.appendChild(modGroupInformation); 
+
+    const modImg = document.createElement('img');
+    modImg.classList.add("modalities__img");
+    modImg.src=`Imagens/${modalities.img[numberIndex]}`;
+    modImg.alt="Imagem dançarina";
+    modGroupInformation.appendChild(modImg);
+
+    const modDescription = document.createElement('div');
+    modDescription.classList.add("flex__column");
+    modDescription.classList.add("modalities__description");
+    modGroupInformation.appendChild(modDescription); 
+
+    modalities.elementCreateDescription.forEach((element,index)=>{
+ 
+        createModalityDescrition (element,modalities.addClass[index],modalities.structureDescription[index],numberIndex, modDescription, modalities)
+    })
+}
+function createModalityDescrition (creatElement,addClass,structure,numberIndex, modDescription, modalities){
+
+    const modElement = document.createElement(`${creatElement}`);
+    modElement.classList.add(`${addClass}`);
+    modElement.innerHTML = modalities[structure][numberIndex];
+    modDescription.appendChild(modElement);
 }
